1145a9a0a01aa5e73378987f6c156d2106cbc449
oxsoftdev <oxsoftdev@gmail.com>
2017-07-21 14:06:22 -0700

    add(setup.py)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='dppy'
+    , version='0.1'
+    , author='sebastian'
+    , author_email='oxsoftdev@gmail.com'
+    , packages=find_packages()
+    , url='https://github.com/oxsoftdev/design-patterns-py'
+    , license='LICENSE.txt'
+)
+
